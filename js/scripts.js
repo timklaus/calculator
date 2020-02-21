08fdaaeffb5162c99d5b1be8bdd4c73f2f867c5a
Student <example@example.com>
2020-02-20 18:48:14 -0800

    add branching to calculator as per lesson plan

--- js/scripts.js
+++ js/scripts.js
@@ -22,11 +22,24 @@ var rem = function(number1, number2) {
 
 // Everything below this line is front-end logic
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
   event.preventDefault();
-  var number1 = parseFloat($("#add1").val());
-  var number2 = parseFloat($("#add2").val());
-  var result = add(number1, number2);
+  var number1 = parseFloat($("#input1").val());
+  var number2 = parseFloat($("#input2").val());
+  var operator = $("input:checked").val();
+  console.log("1st nuber: " + number1);
+  console.log("2nd number: " + number2);
+  console.log("operator: " + operator); 
+  var result;
+  if (operator === "add") {
+    result= add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide (number1, number2);
+  }
   $("#output").text(result);
   });
 });
